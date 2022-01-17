8bb82abf183269aa316afd5125648395655c41fd
Alina Kozyreva <alina0206200119@gmail.com>
2022-01-17 21:37:04 +0300

    enable profile creation after registration

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .models import Profile
 from api.serializers import AccountSerializer
 
 
@@ -10,10 +11,18 @@ class RegisterAccount(APIView):
     def post(self, request):
         account = AccountSerializer(data=request.data)
         if account.is_valid():
-            saved = account.save()
-            print('Saved', account)
-            if saved:
+            saved_account = account.save()
+            if saved_account:
+                profile = Profile(account=saved_account, name=None, surname=None, caption=None, photo=None)
+                profile.save()
                 return Response(status=status.HTTP_201_CREATED)
             return Response(status=status.HTTP_400_BAD_REQUEST)
         return
 
+
+class HomePage(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        account = request.user.username
+        return Response({'user': account})
